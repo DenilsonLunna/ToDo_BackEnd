6b3482ddf61588a7cce9aa33bd8272fc1afbe454
Denilson <denilsonsousa7@outlook.com>
2021-04-29 15:10:44 -0300

    Refatorando codigo, passagem do macadress

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const TaskController = require("../controller/TaskController");
 const TaskValidation = require("../middlewares/TaskValidation");
-const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 
 
@@ -11,12 +10,12 @@ router.put('/:id',TaskValidation,TaskController.update);
 router.get('/:id', TaskController.show);
 router.delete('/:id', TaskController.delete);
 router.put('/:id/:done', TaskController.done);
-router.get('/filter/all',MacaddressValidation, TaskController.all);
-router.get('/filter/late', MacaddressValidation, TaskController.late);
-router.get('/filter/today', MacaddressValidation, TaskController.toDay);
-router.get('/filter/week', MacaddressValidation, TaskController.week);
-router.get('/filter/month', MacaddressValidation, TaskController.month);
-router.get('/filter/year', MacaddressValidation, TaskController.year);
+router.get('/filter/all/:macaddress',TaskController.all);
+router.get('/filter/late/:macaddress', TaskController.late);
+router.get('/filter/today/:macaddress', TaskController.toDay);
+router.get('/filter/week/:macaddress',TaskController.week);
+router.get('/filter/month/:macaddress',TaskController.month);
+router.get('/filter/year/:macaddress',TaskController.year);
 
 
 module.exports = router;
